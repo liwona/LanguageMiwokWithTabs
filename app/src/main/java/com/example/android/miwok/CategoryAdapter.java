0dff33a3db325be06ca5891ad5f39bed1739973a
liwona <liwona@users.noreply.github.com>
2018-04-17 21:33:05 +0200

    removing unneeded Activity classes and activity_category XML

--- app/src/main/java/com/example/android/miwok/CategoryAdapter.java
+++ app/src/main/java/com/example/android/miwok/CategoryAdapter.java
@@ -4,16 +4,26 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+/**
+ * {@link CategoryAdapter} is a {@link FragmentPagerAdapter} that can provide the layout for
+ * each list item based on a data source which is a list of {@link Word} objects.
+ */
+
 public class CategoryAdapter extends FragmentPagerAdapter {
 
     /**
-     * Provides the appropriate {@link Fragment} for a view pager.
+     * Create a new {@link CategoryAdapter} object.
+     *
+     * @param fm is the fragment manager that will keep each fragment's state in the adapter
+     *           across swipes.
      */
-
     public CategoryAdapter(FragmentManager fm) {
         super(fm);
     }
 
+    /**
+     * Return the {@link Fragment} that should be displayed for the given page number.
+     */
     @Override
     public android.support.v4.app.Fragment getItem(int position) {
         if (position == 0) {
@@ -27,6 +37,9 @@ public class CategoryAdapter extends FragmentPagerAdapter {
         }
     }
 
+    /**
+     * Return the total number of pages.
+     */
     @Override
     public int getCount() {
         return 4;
